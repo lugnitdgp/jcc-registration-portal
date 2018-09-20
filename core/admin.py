e1460c545db607182b944ed35e662c824b57a889
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-09-21 01:02:05 +0530

    Modified admin panel

--- core/admin.py
+++ core/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Team
 
-admin.site.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+
+    list_display = (
+        u'team_id',
+        'team_name',
+        'player_one_name',
+        'player_two_name'
+    )
+
+admin.site.register(Team , TeamAdmin)
+
