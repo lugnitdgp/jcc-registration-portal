1f457130d1af3649905725c1f9400ca251bd9ee3
arc9693 <archana.choudhary.9693@gmail.com>
2018-09-21 04:26:05 +0530

    Design form

--- core/forms.py
+++ core/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 
 class RegistrationForm(forms.Form):
-    team_name = forms.CharField(label='Team Name' , max_length=255 , required = True , widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Your Team Name'}))
-    player_one_name = forms.CharField(label='Player 1 Name' , max_length=255 , required = True , widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Player 1 Name'}))
-    player_two_name = forms.CharField(label='Player 2 Name', max_length=255 , required = True , widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Player 2 Name'}))
-
+    team_name = forms.CharField(label='Team Name' , max_length=255 , required = True , widget=forms.TextInput(attrs={'class':'form-control form-control-sm mb-2'}))
+    player_one_name = forms.CharField(label='Player 1 Name' , max_length=255 , required = True , widget=forms.TextInput(attrs={'class':'form-control form-control-sm mb-2'}))
+    player_two_name = forms.CharField(label='Player 2 Name', max_length=255 , required = True , widget=forms.TextInput(attrs={'class':'form-control form-control-sm mb-2'}))
