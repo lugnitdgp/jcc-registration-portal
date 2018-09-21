45da32336c080af53cb7936e41722b4bb6cc9617
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-09-21 15:51:13 +0530

    [FEATURE] Added check to prevent same team names

--- core/views.py
+++ core/views.py
@@ -5,19 +5,28 @@ from .forms import RegistrationForm
 
 
 def index(request):
+
+    is_team_name_taken = False
+
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            new_team = Team(
-                team_name = request.POST['team_name'] ,
-                player_one_name = request.POST['player_one_name'],
-                player_two_name = request.POST['player_two_name']
-            )
-            new_team.save()
-            return render(request , 'success.html')
+            team_name = request.POST['team_name']
+            team = Team.objects.filter(team_name = team_name).first()
+            if not team:
+                new_team = Team(
+                    team_name = request.POST['team_name'],
+                    player_one_name = request.POST['player_one_name'],
+                    player_two_name = request.POST['player_two_name']
+                )
+                new_team.save()
+                return render(request , 'success.html')
+            else:
+                is_team_name_taken = True
     else:
         form = RegistrationForm()
 
-    context = { 'form' : form }
+    context = { 'form' : form , 'is_team_name_taken' : is_team_name_taken }
     return render(request , 'register.html' , context)
+
 
