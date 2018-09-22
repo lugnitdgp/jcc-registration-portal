b0e2e14cf83b1465f6a36c275845bbe6e825f80f
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-09-23 01:51:20 +0530

    Migrated DB to MySQL from SQLite

--- jcc/settings.py
+++ jcc/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'jcc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jcc',
+        'USER': 'root',
+        'PASSWORD':'12345',
+        'HOST':'localhost',
+        'PORT':''
     }
 }
 
