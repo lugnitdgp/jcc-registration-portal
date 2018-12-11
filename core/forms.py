764988db4eaa19c44cac69bce8a0cd114ed4f568
Dinesh Bhagat <45370670+rapperdinesh@users.noreply.github.com>
2018-12-12 01:34:13 +0530

    changed hall_choice to HALL_CHOICE

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+HALL_CHOICES=[
+    ('hall 1','Hall 1'),
+    ('hall 2','Hall 2'),
+    ('hall 3','Hall 3'),
+    ('hall 6','Hall 6'),
+    ('hall 7','Hall 7'),
+    ('hall 11','Hall 11'),
+    ('hall 13','Hall 13'),
+    ('hall 14','Hall 14'),
+]
+
 
 class RegistrationForm(forms.Form):
     team_name = forms.CharField(label='Team Name', max_length=255, required=True, widget=forms.TextInput(
@@ -10,7 +21,7 @@ class RegistrationForm(forms.Form):
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_one_contact = forms.IntegerField(label='Player 1 Contact', required=True, widget=forms.NumberInput(
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
-    player_one_hall = forms.IntegerField(label='Player 1 Hall Number', required=True, widget=forms.NumberInput(
+    player_one_hall = forms.CharField(label='Player 1 Hall Number', required=True, widget=forms.Select(choices=HALL_CHOICES,
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_two_name = forms.CharField(label='Player 2 Name', max_length=255, required=True, widget=forms.TextInput(
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
@@ -18,5 +29,5 @@ class RegistrationForm(forms.Form):
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_two_contact = forms.IntegerField(label='Player 2 Contact', required=True, widget=forms.NumberInput(
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
-    player_two_hall = forms.IntegerField(label='Player 2 Hall Number', required=True, widget=forms.NumberInput(
+    player_two_hall = forms.CharField(label='Player 2 Hall Number', required=True, widget=forms.Select(choices=HALL_CHOICES,
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
