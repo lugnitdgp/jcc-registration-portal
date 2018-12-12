054f726606bd454d883d03ff4703c6dbd263444c
Jayjeet Chakraborty <jc.github@rediffmail.com>
2018-12-12 14:54:33 +0530

    Added Hall 5

--- core/forms.py
+++ core/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 
-HALL_CHOICES=[
-    ('hall 1','Hall 1'),
-    ('hall 2','Hall 2'),
-    ('hall 3','Hall 3'),
-    ('hall 6','Hall 6'),
-    ('hall 7','Hall 7'),
-    ('hall 11','Hall 11'),
-    ('hall 13','Hall 13'),
-    ('hall 14','Hall 14'),
+HALL_CHOICES = [
+    ('hall 1', 'Hall 1'),
+    ('hall 2', 'Hall 2'),
+    ('hall 3', 'Hall 3'),
+    ('hall 5', 'Hall 5'),
+    ('hall 6', 'Hall 6'),
+    ('hall 7', 'Hall 7'),
+    ('hall 11', 'Hall 11'),
+    ('hall 13', 'Hall 13'),
+    ('hall 14', 'Hall 14'),
 ]
 
 
@@ -22,7 +23,7 @@ class RegistrationForm(forms.Form):
     player_one_contact = forms.IntegerField(label='Player 1 Contact', required=True, widget=forms.NumberInput(
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_one_hall = forms.CharField(label='Player 1 Hall Number', required=True, widget=forms.Select(choices=HALL_CHOICES,
-        attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
+                                                                                                       attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_two_name = forms.CharField(label='Player 2 Name', max_length=255, required=True, widget=forms.TextInput(
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_two_email = forms.EmailField(label='Player 2 Email', max_length=255, required=True, widget=forms.EmailInput(
@@ -30,4 +31,5 @@ class RegistrationForm(forms.Form):
     player_two_contact = forms.IntegerField(label='Player 2 Contact', required=True, widget=forms.NumberInput(
         attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
     player_two_hall = forms.CharField(label='Player 2 Hall Number', required=True, widget=forms.Select(choices=HALL_CHOICES,
-        attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
+                                                                                                       attrs={'class': 'form-control form-control-sm mb-2 text-dark'}))
+
