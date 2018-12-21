03a5a393a649aa175b75a88fe8c34491b6c7b9f4
kartik1000 <43112279+kartik1000@users.noreply.github.com>
2018-12-21 22:21:04 +0530

    Added PhoneNumberField() in models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 from django.utils import timezone
 import uuid
 
@@ -12,13 +13,13 @@ class Team(models.Model):
         max_length=255, default="No Player Assigned")
     player_one_email = models.EmailField(
         max_length=255, default="No Email Provided")
-    player_one_contact = models.BigIntegerField(blank=True, null=True)
+    player_one_contact = PhoneNumberField(blank=True)
     player_one_hall = models.CharField(max_length=255, blank=True, null=True)
     player_two_name = models.CharField(
         max_length=255, default="No Player Assigned")
     player_two_email = models.EmailField(
         max_length=255, default="No Email Provided")
-    player_two_contact = models.BigIntegerField(blank=True, null=True)
+    player_two_contact = PhoneNumberField(blank=True)
     player_two_hall = models.CharField(max_length=255, blank=True, null=True)
     timestamp = models.DateTimeField(default=timezone.now)
     unique_team_id = models.CharField(max_length=255, blank=True, null=True)
