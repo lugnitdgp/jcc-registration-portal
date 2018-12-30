4085bd736d2830506cf29d9576a12bc35df2c848
Dinesh Bhagat <45370670+rapperdinesh@users.noreply.github.com>
2018-12-30 16:48:56 +0530

    fixed linting errors

--- core/views.py
+++ core/views.py
@@ -2,12 +2,10 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.conf import settings
 from django.contrib import messages
-from django.core import mail
 from django.core.mail import send_mail
 from .models import Team
 from .forms import RegistrationForm
 from . import sms_service
-
 
 
 def index(request):
@@ -49,12 +47,10 @@ def index(request):
                 sms_service.send_message(
                     team_name, unique_team_id, player_two_contact)
 
-    
                 # **** To send confirmation email to both players ****
 
                 # send_email(subject, message, from_email, to_list, fail_silently=TRUE)
- 
-            
+
                 send_mail(
                     'Successfull Registration in JCC',
                     'Congratulations, You have succesfully registered for JUNIOR CODECRACKER!!! BEST OF LUCK',
@@ -62,8 +58,7 @@ def index(request):
                     [player_one_email, player_two_email],
                 )
 
-               # **** Email part ends here ****
-            
+                # **** Email part ends here ****
                 return render(request, 'success.html', {'unique_team_id': unique_team_id})
 
             else:
